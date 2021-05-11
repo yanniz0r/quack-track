30a1b40475ed32f10739603f039073f80100905d
Yannic Inselmann <yannic.inselmann@gmail.com>
2021-05-11 21:21:27 +0200

    fix: style

---
+++ app/auth/components/login-form.tsx
@@ -0,0 +1,58 @@
+import { AuthenticationError, Link, useMutation, Routes } from "blitz"
+import { Form, FORM_ERROR } from "app/core/components/Form"
+import login from "app/auth/mutations/login"
+import { Login } from "app/auth/validations"
+import Input, { Label } from "../../core/components/input"
+
+type LoginFormProps = {
+  onSuccess?: () => void
+}
+
+export const LoginForm = (props: LoginFormProps) => {
+  const [loginMutation] = useMutation(login)
+
+  return (
+    <div>
+      <Form
+        submitText="Login"
+        schema={Login}
+        initialValues={{ email: "", password: "" }}
+        onSubmit={async (values) => {
+          try {
+            await loginMutation(values)
+            props.onSuccess?.()
+          } catch (error) {
+            if (error instanceof AuthenticationError) {
+              return { [FORM_ERROR]: "Sorry, those credentials are invalid" }
+            } else {
+              return {
+                [FORM_ERROR]:
+                  "Sorry, we had an unexpected error. Please try again. - " + error.toString(),
+              }
+            }
+          }
+        }}
+      >
+        <Label>
+          Email
+          <Input name="email" placeholder="Email" />
+        </Label>
+        <Label>
+          Password
+          <Input name="password" placeholder="Password" type="password" />
+        </Label>
+        <div>
+          <Link href={Routes.ForgotPasswordPage()}>
+            <a>Forgot your password?</a>
+          </Link>
+        </div>
+      </Form>
+
+      <div style={{ marginTop: "1rem" }}>
+        Or <Link href={Routes.SignupPage()}>Sign Up</Link>
+      </div>
+    </div>
+  )
+}
+
+export default LoginForm
